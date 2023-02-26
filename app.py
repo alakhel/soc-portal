7c6b38d07e0a879f8069bf97c987ea34e15c57c8
Amine LAKHEL <mrlakhel@gmail.com>
2023-02-26 17:11:40 +0100

    machine crud

--- app.py
+++ app.py
@@ -15,52 +15,49 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
+
+##############################
+# GENERAL HTML ENDPOINT
+##############################
 #Home Page
 @app.route('/')
 def serve_index():
     return send_file('index.html')
-#Login Page
-@app.route('/login', methods=['GET'])
-def loginPage():
-    return send_file('loginPage.html')
-
-#profile Page
-@app.route('/profile', methods=['GET'])
-def profile():
-    return send_file('profile.html')
-
-#machine Page
-@app.route('/machine', methods=['GET'])
-def machine():
-    return send_file('machine.html')
-#Users admin panel
-@app.route('/userpage')
-def serve_user_page():
-    return send_file('user.html')
-
-#Login endpoint
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
-    if not username or not password:
-        return jsonify({'error': 'Missing username or password'}), 400
-
-    # Get user from the database
-    user = User.query.filter_by(login=username).first()
-    if not user or not check_password_hash(user.password_hash, password):
-        return jsonify({'error': 'Invalid username or password'}), 401
-
-    # Create access token for the user
-    access_token = create_access_token(identity=user.id)
-    return jsonify({'login': True, 'access_token': access_token}), 200
-
 #Protected Dashboard
 @app.route('/dashboard', methods=['GET'])
 #@jwt_required
 def dashboard():
      return send_file('dashboard.html')
 
+###############################
+# USER HTML ENDPOINT
+###############################
+#Users admin panel
+@app.route('/userpage')
+def serve_user_page():
+    return send_file('user.html')
+#profile Page
+@app.route('/profile', methods=['GET'])
+def profile():
+    return send_file('profile.html')
+#Login Page
+@app.route('/login', methods=['GET'])
+def loginPage():
+    return send_file('loginPage.html')
+
+
+###############################
+# MACHINE HTML ENDPOINT
+###############################
+#machine Page
+@app.route('/machine', methods=['GET'])
+def machine():
+    return send_file('machine.html')
+
+
+####################################
+#    USER CRUD ENDPOINT            #
+####################################
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     prenom = db.Column(db.String(50))
@@ -81,7 +78,24 @@ class User(db.Model):
 def create_tables():
     with app.app_context():
         db.create_all()
+#-----------------------------------#
+#Login endpoint
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username or not password:
+        return jsonify({'error': 'Missing username or password'}), 400
 
+    # Get user from the database
+    user = User.query.filter_by(login=username).first()
+    if not user or not check_password_hash(user.password_hash, password):
+        return jsonify({'error': 'Invalid username or password'}), 401
+
+    # Create access token for the user
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'login': True, 'access_token': access_token}), 200
+#-----------------------------------#
 # Create a new user
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -91,7 +105,7 @@ def create_user():
     db.session.add(user)
     db.session.commit()
     return jsonify({'message': 'User created successfully.'}), 201
-
+#-----------------------------------#
 # Get all users
 @app.route('/users', methods=['GET'])
 def get_all_users():
@@ -101,7 +115,7 @@ def get_all_users():
         user_data = {'id': user.id, 'prenom': user.prenom, 'nom': user.nom, 'login': user.login, 'groupe': user.groupe}
         result.append(user_data)
     return jsonify(result), 200
-
+ #-----------------------------------#
 # Get a specific user
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
@@ -111,7 +125,7 @@ def get_user(user_id):
         return jsonify(user_data), 200
     else:
         return jsonify({'message': 'User not found.'}), 404
-
+#-----------------------------------#
 # Update a user
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
@@ -132,7 +146,7 @@ def update_user(user_id):
         return jsonify({'message': 'User updated successfully.'}), 200
     else:
         return jsonify({'message': 'User not found.'}), 404
-
+#-----------------------------------#
 # Delete a user
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
@@ -143,6 +157,63 @@ def delete_user(user_id):
         return jsonify({'message': 'User deleted successfully.'}), 200
     else:
         return jsonify({'message': 'User not found.'}), 404
+
+
+###############################
+# MACHINE API ENDPOINT
+###############################
+
+class Machine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hostname = db.Column(db.String(255), nullable=False)
+    ip = db.Column(db.String(15), nullable=False)
+    group = db.Column(db.String(50), nullable=False)
+#-----------------------------------#
+@app.route('/machines', methods=['GET'])
+def get_machines():
+    machines = Machine.query.all()
+    return jsonify([{'id': m.id, 'hostname': m.hostname, 'ip': m.ip, 'group': m.group} for m in machines])
+#-----------------------------------#
+@app.route('/machines/<int:machine_id>', methods=['GET'])
+def get_machine(machine_id):
+    machine = Machine.query.get(machine_id)
+    if machine:
+        return jsonify({'id': machine.id, 'hostname': machine.hostname, 'ip': machine.ip, 'group': machine.group})
+    else:
+        return jsonify({'error': 'Machine not found'})
+#-----------------------------------#
+@app.route('/machines', methods=['POST'])
+def add_machine():
+    data = request.get_json()
+    machine = Machine(hostname=data['hostname'], ip=data['ip'], group=data['group'])
+    db.session.add(machine)
+    db.session.commit()
+    return jsonify({'id': machine.id, 'hostname': machine.hostname, 'ip': machine.ip, 'group': machine.group})
+#-----------------------------------#
+@app.route('/machines/<int:machine_id>', methods=['PUT'])
+def update_machine(machine_id):
+    machine = Machine.query.get(machine_id)
+    if machine:
+        data = request.get_json()
+        machine.hostname = data['hostname']
+        machine.ip = data['ip']
+        machine.group = data['group']
+        db.session.commit()
+        return jsonify({'id': machine.id, 'hostname': machine.hostname, 'ip': machine.ip, 'group': machine.group})
+    else:
+        return jsonify({'error': 'Machine not found'})
+#-----------------------------------#
+@app.route('/machines/<int:machine_id>', methods=['DELETE'])
+def delete_machine(machine_id):
+    machine = Machine.query.get(machine_id)
+    if machine:
+        db.session.delete(machine)
+        db.session.commit()
+        return jsonify({'message': 'Machine deleted successfully'})
+    else:
+        return jsonify({'error': 'Machine not found'})
+
+
 
 if __name__ == '__main__':
     create_tables()
--- readme.md
+++ readme.md
@@ -32,3 +32,11 @@ GET /users: Returns a list of all users.
 GET /users/<user_id>: Returns a specific user by ID.
 PUT /users/<user_id>: Updates a specific user by ID.
 DELETE /users/<user_id>: Deletes a specific user by ID.
+
+### Machine Endpoint API
+
+GET /machines: Retrieve a list of all machines in the database.
+GET /machines/:id: Retrieve a specific machine by ID.
+POST /machines: Create a new machine.
+PUT /machines/:id: Update an existing machine by ID.
+DELETE /machines/:id: Delete a machine by ID.
